b4f4a72adaf4924442c0f561182726681ef35f36
Olivier Duchateau <olivierd@FreeBSD.org>
2015-11-28 14:42:01 +0000

    Add deskutils/qlipper
    git-svn-id: https://subversion.assembla.com/svn/lxqt/tags/0.10@109 a3fa71b5-6099-4007-a917-a7dc97f501c3

---
+++ deskutils/qlipper/files/patch-src_qlipperpreferences.h
@@ -0,0 +1,10 @@
+--- src/qlipperpreferences.h.orig	2015-10-26 18:31:05 UTC
++++ src/qlipperpreferences.h
+@@ -21,6 +21,7 @@ with this program; if not, write to the 
+ #define QLIPPERPREFERENCES_H
+ 
+ #include <QtCore/QSettings>
++#include <QDataStream>
+ 
+ #include "qlipperitem.h"
+ 
