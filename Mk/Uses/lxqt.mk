a406991b25f9cee2042b23887a5bc3319da0e2ee
Olivier Duchateau <olivierd@FreeBSD.org>
2016-09-20 16:54:31 +0000

    Remove reference to x11-toolkits/qtermwidget
    git-svn-id: https://subversion.assembla.com/svn/lxqt/tags/0.11@172 a3fa71b5-6099-4007-a917-a7dc97f501c3

--- Mk/Uses/lxqt.mk
+++ Mk/Uses/lxqt.mk
@@ -12,7 +12,6 @@
 # globalkeys	- Keyboard shortcuts daemon
 # libfmqt	- Libfm Qt bindings
 # lxqt		- LXQt core library
-# qterm		- Terminal widget library
 # qtxdg		- Qt implementation of freedesktop.org xdg specs
 #
 # MAINTAINER: olivierd@FreeBSD.org
@@ -47,7 +46,7 @@ PLIST_SUB+=	LXQT_INCLUDEDIR="include/lxqt" \
 CMAKE_ARGS+=	-DCMAKE_INSTALL_MANDIR=${MANDIRS}
 
 # Available LXQt components are:
-_USE_LXQT_ALL=	globalkeys libfmqt lxqt qterm qtxdg
+_USE_LXQT_ALL=	globalkeys libfmqt lxqt qtxdg
 
 globalkeys_LIB_DEPENDS=	liblxqt-globalkeys.so:x11/lxqt-globalkeys
 globalkeys_USE_LXQT_REQ=	lxqt
@@ -56,8 +55,6 @@ libfmqt_LIB_DEPENDS=	libfm-qt.so:x11/libfm-qt
 
 lxqt_LIB_DEPENDS=	liblxqt.so:devel/liblxqt
 lxqt_USE_LXQT_REQ=	qtxdg
-
-qterm_LIB_DEPENDS=	libqtermwidget5.so:x11-toolkits/qtermwidget
 
 qtxdg_LIB_DEPENDS=	libQt5Xdg.so:devel/libqtxdg
 
