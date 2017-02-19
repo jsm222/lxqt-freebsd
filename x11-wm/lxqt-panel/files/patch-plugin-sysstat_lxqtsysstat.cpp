37c629941481efd92c64d45ef8a9669d139e9755
Jesper Schmitz Mouridsen <jesper@schmitz.computer>
2017-02-19 22:18:09 +0100

    Work in progress libsysstat support merge https://subversion.assembla.com/svn/lxqt/trunk@345 a3fa71b5-6099-4007-a917-a7dc97f501c3

--- x11-wm/lxqt-panel/files/patch-plugin-sysstat_lxqtsysstat.cpp
+++ x11-wm/lxqt-panel/files/patch-plugin-sysstat_lxqtsysstat.cpp
@@ -1,90 +1,84 @@
 --- plugin-sysstat/lxqtsysstat.cpp.orig	2017-01-01 22:27:14 UTC
 +++ plugin-sysstat/lxqtsysstat.cpp
-@@ -27,7 +27,6 @@
- 
- #include "lxqtsysstat.h"
- #include "lxqtsysstatutils.h"
--
- #include <SysStat/CpuStat>
- #include <SysStat/MemStat>
- #include <SysStat/NetStat>
-@@ -323,12 +322,20 @@ void LXQtSysStatContent::updateSettings(
+@@ -320,6 +320,7 @@ void LXQtSysStatContent::updateSettings(
+         {
+             if (mDataType == "CPU")
+             {
++#if defined(Q_OS_LINUX)
                  if (mUseFrequency)
                  {
                      qobject_cast<SysStat::CpuStat*>(mStat)->setMonitoring(SysStat::CpuStat::LoadAndFrequency);
-+#ifdef HAVE_SYSCTL_H
-+                    connect(qobject_cast<SysStat::CpuStat*>(mStat), SIGNAL(update(float, float, float, float, float, ulong)), this, SLOT(cpuUpdate(float, float, float, float, float, ulong)));
-+#else
-                     connect(qobject_cast<SysStat::CpuStat*>(mStat), SIGNAL(update(float, float, float, float, float, uint)), this, SLOT(cpuUpdate(float, float, float, float, float, uint)));
-+#endif
-                 }
-                 else
-                 {
+@@ -330,6 +331,10 @@ void LXQtSysStatContent::updateSettings(
                      qobject_cast<SysStat::CpuStat*>(mStat)->setMonitoring(SysStat::CpuStat::LoadOnly);
--                    connect(qobject_cast<SysStat::CpuStat*>(mStat), SIGNAL(update(float, float, float, float)), this, SLOT(cpuUpdate(float, float, float, float)));
-+#ifdef HAVE_SYSCTL_H
-+                    connect(qobject_cast<SysStat::CpuStat*>(mStat), SIGNAL(update(float, float, float, float,float)), this, SLOT(cpuUpdate(float, float, float, float,float)));
-+#else
-+                    connect(qobject_cast<SysStat::CpuStat*>(mStat), SIGNAL(update(float, float, float,float)), this, SLOT(cpuUpdate(float, float, float,float)));
-+#endif
+                     connect(qobject_cast<SysStat::CpuStat*>(mStat), SIGNAL(update(float, float, float, float)), this, SLOT(cpuUpdate(float, float, float, float)));
                  }
++#elif defined(Q_OS_FREEBSD)
++                qobject_cast<SysStat::CpuStat*>(mStat)->setMonitoring(SysStat::CpuStat::LoadOnly);
++                connect(qobject_cast<SysStat::CpuStat*>(mStat), SIGNAL(update(float, float, float, float, float)), this, SLOT(cpuUpdate(float, float, float, float, float)));
++#endif
              }
              else if (mDataType == "Memory")
-@@ -385,7 +392,18 @@ void LXQtSysStatContent::clearLine()
-     for (int i = 0; i < 100; ++i)
+             {
+@@ -386,6 +391,7 @@ void LXQtSysStatContent::clearLine()
          reinterpret_cast<QRgb*>(mHistoryImage.scanLine(i))[mHistoryOffset] = bg;
  }
--
-+#ifdef HAVE_SYSCTL_H
-+void LXQtSysStatContent::cpuUpdate(float user, float nice, float system, float idle,float other, ulong freq)
+ 
++#if defined(Q_OS_LINUX)
+ void LXQtSysStatContent::cpuUpdate(float user, float nice, float system, float other, float frequencyRate, uint)
+ {
+     int y_system = static_cast<int>(system * 100.0 * frequencyRate);
+@@ -478,6 +484,54 @@ void LXQtSysStatContent::cpuUpdate(float
+ 
+     update(0, mTitleFontPixelHeight, width(), height() - mTitleFontPixelHeight);
+ }
++#elif defined(Q_OS_FREEBSD)
++void LXQtSysStatContent::cpuUpdate(float user, float nice, float system, float idle, float other)
 +{
 +    int y_system = static_cast<int>(system * 100.0);
 +    int y_user   = static_cast<int>(user   * 100.0);
 +    int y_nice   = static_cast<int>(nice   * 100.0);
 +    int y_idle   = static_cast<int>(idle   * 100.0);
 +    int y_other  = static_cast<int>(other  * 100.0);
-+    int y_freq   = static_cast<int>(freq);
-+    toolTipInfo(tr("system: %1%<br>user: %2%<br>nice: %3%<br>idle: %4%<br>other: %5%<br>freq: %6MHz", "CPU tooltip information")
-+            .arg(y_system).arg(y_user).arg(y_nice).arg(y_idle).arg(y_other).arg(y_freq));
-+#else
- void LXQtSysStatContent::cpuUpdate(float user, float nice, float system, float other, float frequencyRate, uint)
- {
-     int y_system = static_cast<int>(system * 100.0 * frequencyRate);
-@@ -396,7 +414,7 @@ void LXQtSysStatContent::cpuUpdate(float
++
++    float used  = (user + nice);
++    float cpu_percent  = (used * 100) / (used + system + idle + other);
++
++    toolTipInfo(tr("%1%", "CPU tooltip information")
++            .arg(cpu_percent));
++
++    y_system = clamp(y_system, 0, 99);
++    y_user   = clamp(y_user + y_nice, 0, 99);
++    y_nice   = clamp(y_nice, 0, 99);
++    y_other  = clamp(y_other + y_idle, 0, 99);
++
++    clearLine();
++    QPainter painter(&mHistoryImage);
++    if (y_system != 0)
++    {
++        painter.setPen(mColours.cpuSystemColour);
++        painter.drawLine(mHistoryOffset, y_system, mHistoryOffset, 0);
++    }
++    if (y_user != y_system)
++    {
++        painter.setPen(mColours.cpuUserColour);
++        painter.drawLine(mHistoryOffset, y_user, mHistoryOffset, y_system);
++    }
++    if (y_nice != y_user)
++    {
++        painter.setPen(mColours.cpuNiceColour);
++        painter.drawLine(mHistoryOffset, y_nice, mHistoryOffset, y_user);
++    }
++    if (y_other != y_nice)
++    {
++        painter.setPen(mColours.cpuOtherColour);
++        painter.drawLine(mHistoryOffset, y_other, mHistoryOffset, y_nice);
++    }
++
++    mHistoryOffset = (mHistoryOffset + 1) % width();
++
++    update(0, mTitleFontPixelHeight, width(), height() - mTitleFontPixelHeight);
++}
++#endif
  
-     toolTipInfo(tr("system: %1%<br>user: %2%<br>nice: %3%<br>other: %4%<br>freq: %5%", "CPU tooltip information")
-             .arg(y_system).arg(y_user).arg(y_nice).arg(y_other).arg(y_freq));
--
-+#endif
-     y_system = clamp(y_system, 0, 99);
-     y_user   = clamp(y_user + y_system, 0, 99);
-     y_nice   = clamp(y_nice + y_user  , 0, 99);
-@@ -436,16 +454,26 @@ void LXQtSysStatContent::cpuUpdate(float
-     update(0, mTitleFontPixelHeight, width(), height() - mTitleFontPixelHeight);
- }
- 
-+#ifdef HAVE_SYSCTL_H
-+void LXQtSysStatContent::cpuUpdate(float user, float nice, float system, float idle,float other)
-+#else
- void LXQtSysStatContent::cpuUpdate(float user, float nice, float system, float other)
-+#endif
+ void LXQtSysStatContent::memoryUpdate(float apps, float buffers, float cached)
  {
-     int y_system = static_cast<int>(system * 100.0);
-     int y_user   = static_cast<int>(user   * 100.0);
-     int y_nice   = static_cast<int>(nice   * 100.0);
-+#ifdef HAVE_SYSCTL_H
-+    int y_idle   = static_cast<int>(idle   * 100.0);
-+#endif
-     int y_other  = static_cast<int>(other  * 100.0);
--
-+#ifdef HAVE_SYSCTL_H
-+    toolTipInfo(tr("system: %1%<br>user: %2%<br>nice: %3%<br>idle: %4%<br>other: %5%<br>freq: n/a", "CPU tooltip information")
-+            .arg(y_system).arg(y_user).arg(y_nice).arg(y_idle).arg(y_other));
-+#else
-     toolTipInfo(tr("system: %1%<br>user: %2%<br>nice: %3%<br>other: %4%<br>freq: n/a", "CPU tooltip information")
-             .arg(y_system).arg(y_user).arg(y_nice).arg(y_other));
--
-+#endif
-     y_system = clamp(y_system, 0, 99);
-     y_user   = clamp(y_user + y_system, 0, 99);
-     y_nice   = clamp(y_nice + y_user, 0, 99);
--- x11-wm/lxqt-panel/files/patch-plugin-sysstat_lxqtsysstat.h
+++ x11-wm/lxqt-panel/files/patch-plugin-sysstat_lxqtsysstat.h
@@ -1,28 +1,14 @@
 --- plugin-sysstat/lxqtsysstat.h.orig	2017-01-01 22:27:14 UTC
 +++ plugin-sysstat/lxqtsysstat.h
-@@ -30,7 +30,7 @@
- 
- #include "../panel/ilxqtpanelplugin.h"
- #include "lxqtsysstatconfiguration.h"
--
-+#include "config.h"
- #include <QLabel>
- 
- 
-@@ -137,8 +137,16 @@ protected:
+@@ -137,8 +137,12 @@ protected:
      void resizeEvent(QResizeEvent *);
  
  protected slots:
-+#ifdef HAVE_SYSCTL_H
-+    void cpuUpdate(float user, float nice, float system, float idle,float other, ulong freq);
-+#else
++#if defined(Q_OS_LINUX)
      void cpuUpdate(float user, float nice, float system, float other, float frequencyRate, uint frequency);
--    void cpuUpdate(float user, float nice, float system, float other);
-+#endif
-+#ifdef HAVE_SYSCTL_H
-+    void cpuUpdate(float user, float nice, float system, float idle,float other);
-+#else
-+    void cpuUpdate(float user, float nice, float system,float other);
+     void cpuUpdate(float user, float nice, float system, float other);
++#elif defined(Q_OS_FREEBSD)
++    void cpuUpdate(float user, float nice, float system, float idle, float other);
 +#endif
      void memoryUpdate(float apps, float buffers, float cached);
      void swapUpdate(float used);
